d9756bf7def5383ac0ab5a5f71599481a8bc9b26
newbie <newbie@labs.(none)>
2012-12-10 23:30:34 +0700

    Push Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tugas::Application.config.secret_token = '7de500015f3d84cdd36feb3dc2b40f0e4f445b815288388b44b257163d5ce65f5546790127896fe1cf8f9d69c58d2af32e4ecf576230abf0c623972d144f8d86'
